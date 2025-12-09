ebc4cc35001fcb61f4fc74265e86ad94eeac0571
Saiyed Umair Ashraf <66516259+SaiyedUmairAshraf07@users.noreply.github.com>
2025-12-09 12:10:34 +0530

    Add files via upload

---
+++ BatteryIndicator/BatteryIndicatorTests/BatteryIndicatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryIndicatorTests.swift
+//  BatteryIndicatorTests
+//
+//  Created by cedcoss on 04/12/25.
+//
+
+import Testing
+@testable import BatteryIndicator
+
+struct BatteryIndicatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
